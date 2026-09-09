72b4eca8264c4a88b438bfe932b99bdd93d1d3db
tiffasmart-droid <tiffasmart@gmail.com>
2026-09-09 09:55:11 +0800

    Update config.js

--- 東港_0912_五人共享分帳_GitHub版/config.js
+++ 東港_0912_五人共享分帳_GitHub版/config.js
@@ -3,5 +3,5 @@ window.TRIP_CONFIG = Object.freeze({
   publishableKey: 'sb_publishable_LgbhTvA5bGUPIdz3Wfs3HQ_DJrLtojx',
   room: '0912',
   // 固定編號 0～4；更名時請勿調換順序，並重新部署給所有手機。
-  names: ['粉粉', '旅伴2', '旅伴3', '旅伴4', '旅伴5']
+  names: ['粉粉', '夢肉', '妗庭', '費玉', '筠紋']
 });
